3f6d3c2633fe59724701de9d5c3c9871aeba8030
Minh20812 <blossworld12@gmail.com>
2024-08-21 23:06:35 +0700

    Learn Redux Query CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Counter } from "./features/counter/counter";
+import AllProducts from "./component/AllProduct";
+import AddNewProduct from "./component/AddNewProduct";
+import DeleteProduct from "./component/DeleteProduct";
+import SpecificProduct from "./component/SpecificProduct";
+import UpdateProduct from "./component/UpdateProduct";
 
 export default function App() {
   return (
     <>
-      <Counter />
+      {/* <Counter /> */}
+      {/* <AllProducts /> */}
+      {/* <AddNewProduct /> */}
+      {/* <DeleteProduct /> */}
+      {/* <SpecificProduct /> */}
+      <UpdateProduct />
     </>
   );
 }
